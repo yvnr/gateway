0f31a6bc413a8b7f639ef6663917362e697ff2c7
Phanindra Pvs <vpamidimukka@umass.edu>
2022-12-08 05:54:49 -0500

    added http function api

---
+++ functions/src/api/index.ts
@@ -0,0 +1,41 @@
+import * as express from 'express';
+import {Router} from 'express';
+import applicationRoute from './application';
+import experienceRoute from './experience';
+import analyticsRoute from './analytics';
+import userRoute from './user';
+import universityRoute from './university';
+import * as cors from 'cors';
+import firebaseAuth from './middleware/firebaseAuth';
+
+import * as functions from 'firebase-functions';
+
+const app = express();
+const apiRoute = Router();
+
+app.use(cors());
+app.use((req, res, next) => {
+  console.log('url', req.originalUrl);
+  console.log('method', req.method);
+  next();
+});
+
+const authRoutes = [
+  '/api/application',
+  '/api/experience',
+  '/api/analytics',
+  '/api/user',
+];
+
+
+app.use(authRoutes, firebaseAuth);
+
+app.use('/api', apiRoute);
+
+apiRoute.use('/application', applicationRoute);
+apiRoute.use('/experience', experienceRoute);
+apiRoute.use('/analytics', analyticsRoute);
+apiRoute.use('/user', userRoute);
+apiRoute.use('/university', universityRoute);
+
+export const api = functions.https.onRequest(app);
